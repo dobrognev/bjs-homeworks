024aac1a44b605648236dc11fa54206ec9040107
dobrognev <kroto911@gmail.com>
2020-09-17 16:16:49 +0300

    html

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -4,22 +4,30 @@ class PrintEditionItem {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this.state = 70;
+        this.state = 100;
         this.type = null;
     }
 
     fix() {
+        return this.state * 1.5;
+    }
 
-        let state = this.state * 1.5;
-        return state;
+    set stateValue(_state_) {
+        if (this.state < 0) {
+            state = 0
+        } else if (this.state > 100) {
+            state = 100
+        } else state = this.state
+    }
+    get stateValue() {
+        return this._state_;
     }
 }
 
 const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
 
-console.log(sherlock.name) //
-console.log(sherlock.releaseDate); //2019
-console.log(sherlock.state); //100
+console.log(sherlock.name) // Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе
+console.log(sherlock.releaseDate); // 2019
+console.log(sherlock.state); // 100
 sherlock.fix();
-console.log(sherlock.state); //100
-
+console.log(sherlock.state); // 100
